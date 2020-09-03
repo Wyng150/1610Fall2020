5ba9a449ffd72e5a7c67b1454f511de45b808600
Russell NIelsen <Drawnbyruss@gmail.com>
2020-09-03 00:05:22 -0600

    Added new scripts

--- github repo/Cyborg shrimp/Assets/scripts/MoverScript.cs
+++ github repo/Cyborg shrimp/Assets/scripts/MoverScript.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MoverScript : MonoBehaviour
 {
-    public float speed = 3f;
+    public float speed = 6f;
     public float x, y, z;
     // Start is called before the first frame update
     void Start()
