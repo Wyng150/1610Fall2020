549dddebff894633206370d2878cdf0af42760d1
Russell NIelsen <Drawnbyruss@gmail.com>
2020-09-04 21:44:52 -0600

    Finally getting this

--- github repo/Cyborg shrimp/Assets/scripts/CharacterMover.cs
+++ github repo/Cyborg shrimp/Assets/scripts/CharacterMover.cs
@@ -5,16 +5,33 @@ using UnityEngine;
 
 public class CharacterMover : MonoBehaviour
 {
-
-
-
-
-public float speed = 7f;
+    
+public float speed = 3f;
 
 public int score = 100;
         // Update is called once per frame
     private void Update()
     {
-        transform.Translate(speed*Time.deltaTime ,0,0);
+        var vInput = speed * Time.deltaTime*Input.GetAxis("Vertical");
+        var hInput = speed * Time.deltaTime*Input.GetAxis("Horizontal")  ;
+        transform.Translate(hInput ,vInput,0);
+    }
+
+    public void Up()
+    {
+        transform.Translate(0,speed*Time.deltaTime,0 );
+    }
+
+    public void Down()
+    {
+        transform.Translate(0, -speed*Time.deltaTime, 0);
+    }
+    public void Left()
+    {
+        transform.Translate(-speed*Time.deltaTime, 0, 0);
+    }
+    public void Right()
+    {
+        transform.Translate(speed*Time.deltaTime, 0, 0);
     }
 }
