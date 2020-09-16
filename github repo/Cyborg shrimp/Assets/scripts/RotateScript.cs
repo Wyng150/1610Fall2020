0159d3a1de4e2c4e47dfca879cc2792205d93904
Russell NIelsen <Drawnbyruss@gmail.com>
2020-09-16 09:44:23 -0600

    built rotation

---
+++ github repo/Cyborg shrimp/Assets/scripts/RotateScript.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class RotateScript : MonoBehaviour
+{
+    public float speed = 5f;
+
+    [FormerlySerializedAs("RotationVector3")]
+    public Vector3 rotationVector3;
+
+    private void Update()
+    {
+        rotationVector3.y = speed * Time.deltaTime;
+        transform.Rotate(rotationVector3);
+
+    }
+
+}
---
+++ github repo/Cyborg shrimp/Assets/scripts/ValuePoint.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+public class ValuePoint : MonoBehaviour
+{
+   public int value;
+
+   private void OnTriggerEnter(Collider other)
+   {
+      value++;
+      print (value);
+   }
+}
