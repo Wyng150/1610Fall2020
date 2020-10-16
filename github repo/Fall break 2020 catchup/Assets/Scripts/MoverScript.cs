feba390e26b8713fd98184fdc02e19aa27a11d8a
Russell NIelsen <Drawnbyruss@gmail.com>
2020-10-16 16:37:02 -0600

    Made a new game and mover script

---
+++ github repo/Fall break 2020 catchup/Assets/Scripts/MoverScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoverScript : MonoBehaviour
+{
+    public float speed = 1f;
+    public float x, y, z;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("This is my World Now!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        x = speed*Input.GetAxis("Horizontal");
+        y = speed*Input.GetAxis("Vertical");
+        transform.Translate(x,y,z);
+    }
+}
