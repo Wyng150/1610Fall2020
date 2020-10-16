78e7c77e4354503dda44553cc39553e1605c0d0f
Russell NIelsen <Drawnbyruss@gmail.com>
2020-10-16 13:51:32 -0600

    still trying

---
+++ github repo/Cyborg shrimp/Assets/scripts/destroyer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroyer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void S
+}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
