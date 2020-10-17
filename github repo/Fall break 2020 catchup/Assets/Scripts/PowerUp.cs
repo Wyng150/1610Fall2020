e171257543f550477b17d325274e9d30895ad693
Russell NIelsen <Drawnbyruss@gmail.com>
2020-10-16 21:12:01 -0600

    added power up

---
+++ github repo/Fall break 2020 catchup/Assets/Scripts/PowerUp.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class PowerUp : MonoBehaviour
+{
+    public int score;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        score++;
+        print(score);
+    }
+}
