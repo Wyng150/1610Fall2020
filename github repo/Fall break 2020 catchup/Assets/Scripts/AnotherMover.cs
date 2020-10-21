b34ccbbce725860c1537b3c22699d95bc451c8fc
Russell NIelsen <Drawnbyruss@gmail.com>
2020-10-20 23:25:10 -0600

    Expertimented with more movers

---
+++ github repo/Fall break 2020 catchup/Assets/Scripts/AnotherMover.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnotherMover : MonoBehaviour
+{
+    public float moveSpeed;
+    private Vector3 moveDirection;
+
+    private void Update()
+    {
+        moveDirection.x = moveSpeed * Time.deltaTime;
+    }
+}
--- github repo/Fall break 2020 catchup/Assets/Scripts/TransformScript.cs
+++ github repo/Fall break 2020 catchup/Assets/Scripts/TransformScript.cs
@@ -3,12 +3,15 @@
 public class TransformScript : MonoBehaviour
 {
     public float speed = 10f;
+    public int count;
     public Vector3 RotationVector3;
+    public string password;
     private void Update()
+    
     {
-        if (expr)
+        if (password == "frank")
         {
-            
+            print("correct");
         }
         RotationVector3.y = speed = Time.deltaTime;
         transform.Rotate(RotationVector3);
