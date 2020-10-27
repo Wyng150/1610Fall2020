25e34da7e22a26a32c96a3267c82e80ad456a5f4
Russell NIelsen <Drawnbyruss@gmail.com>
2020-10-27 17:10:49 -0600

    Events and particles

--- github repo/Fall break 2020 catchup/Assets/Scripts/HealthPowerUp.cs
+++ github repo/Fall break 2020 catchup/Assets/Scripts/HealthPowerUp.cs
@@ -1,13 +1,14 @@
-﻿using System;
-using UnityEngine;
-
+﻿using UnityEngine;
+using UnityEngine.Events;
 public class HealthPowerUp : MonoBehaviour
 {
     public int health;
-
+    public UnityEvent healthPowerUp;
+    
     private void OnTriggerEnter(Collider other)
     {
         health++;
         print(health);
+        healthPowerUp.Invoke();
     }
 }
--- github repo/Fall break 2020 catchup/Assets/Scripts/PowerUp.cs
+++ github repo/Fall break 2020 catchup/Assets/Scripts/PowerUp.cs
@@ -1,12 +1,13 @@
 ﻿using UnityEngine;
-
+using UnityEngine.Events;
 public class PowerUp : MonoBehaviour
 {
     public int score;
-
+    public UnityEvent powerUpEvent;
     private void OnTriggerEnter(Collider other)
     {
         score++;
         print(score);
+        powerUpEvent.Invoke();
     }
 }
