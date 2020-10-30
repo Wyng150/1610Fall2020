ec2b9a12d8cd90b319a3f9819e1cf4577a961517
Russell NIelsen <Drawnbyruss@gmail.com>
2020-10-29 18:24:06 -0600

    Worked on colliders

---
+++ github repo/Fall break 2020 catchup/Assets/Scripts/CharacterMover.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+public class CharacterMover : MonoBehaviour
+{
+    public CharacterController controller;
+    public Vector3 moveDirection;
+
+    private void Update()
+    {
+        controller.Move(moveDirection);
+    }
+}
