0ca6571418fcc1fc2f585d52ff7076cbe5d032bb
Russell NIelsen <Drawnbyruss@gmail.com>
2020-11-02 15:15:16 -0700

    Jump script with gravity

--- github repo/Fall break 2020 catchup/Assets/Scripts/CharacterMover.cs
+++ github repo/Fall break 2020 catchup/Assets/Scripts/CharacterMover.cs
@@ -1,12 +1,30 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 public class CharacterMover : MonoBehaviour
 {
     public CharacterController controller;
-    public Vector3 moveDirection;
+    public float moveSpeed = 5f, gravity= -9.81f, jumpForce =5f;
+    
+    private Vector3 moveDirection;
+    private float yDirection;
 
     private void Update()
     {
-        controller.Move(moveDirection);
+        var moveSpeedInput = moveSpeed * Input.GetAxis("Horizontal");
+        moveDirection.Set(moveSpeedInput,yDirection,0);
+
+        yDirection += gravity * Time.deltaTime;
+
+        if (controller.isGrounded && moveDirection.y <0)
+        {
+            yDirection = -1f;
+        }
+
+        if (Input.GetButtonDown("Jump"));
+        {
+            yDirection = jumpForce;
+        }
+        
+        var movement = moveDirection * Time.deltaTime;
+        controller.Move(movement);
     }
 }
